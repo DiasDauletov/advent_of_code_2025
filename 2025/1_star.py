9d54bcdde416d06cba4b020c87aa91fe8beac169
DiasDauletov <dias.daultv+github@gmail.com>
2025-12-01 16:00:18 +0000

    Add Day 1 solutions

--- 2025/1_star.py
+++ 2025/1_star.py
@@ -1,4 +1,4 @@
-# https://adventofcode.com/2025/day/1
+# https://adventofcode.com/2025/day/1#part2
 
 dial = 50
 counter = 0
@@ -11,10 +11,17 @@ while True:
     direction = text[0]
     num_of_rotations = int(text[1:])
 
+    counter += int(num_of_rotations / 100)
+    num_of_rotations %= 100
+
+    prev_dial = dial
+
     dial += num_of_rotations if direction == 'R' else -num_of_rotations
     dial %= 100
 
     if dial == 0:
+        counter += 1
+    elif prev_dial != 0 and ((direction == 'R' and prev_dial > dial) or (direction == 'L' and prev_dial < dial)):
         counter += 1
 
 print(counter)
